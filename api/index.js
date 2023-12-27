9b18d6d1e661909a3c690b997f16026bec20150a
shoaibakhtar9708 <71517604+shoaibakhtar9708@users.noreply.github.com>
2023-12-27 17:19:00 +0530

    Database connected

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Database conneted");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.send("home page.");
