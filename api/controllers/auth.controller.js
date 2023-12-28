f81d227435d57c6161f7604de0eb2e39ac5df913
shoaibakhtar9708 <71517604+shoaibakhtar9708@users.noreply.github.com>
2023-12-28 12:23:01 +0530

    create error middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptyjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptyjs.hashSync(password, 10);
   //console.log(hashedPassword);
@@ -10,6 +11,7 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.status(201).json("user created successfully");
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
+    // next(errorHandler(500, "error from function"));
   }
 };
--- api/index.js
+++ api/index.js
@@ -21,6 +21,17 @@ mongoose
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "internal server error";
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
